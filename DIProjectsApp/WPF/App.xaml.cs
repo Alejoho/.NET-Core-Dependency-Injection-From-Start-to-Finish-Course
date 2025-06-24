f3d644b4df72207ebcdf017f2a69ed693d93ac5a
Alejoho <alejoreilly96@gmail.com>
2025-06-24 07:46:32 -0400

    Lesson: WPF (.NET Core)

---
+++ DIProjectsApp/WPF/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using DIDemoLib;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = Host.CreateApplicationBuilder();
+
+            builder.Services.AddTransient<IMessages, Messages>();
+            builder.Services.AddSingleton<MainWindow>();
+
+            var app = builder.Build();
+
+            var mainFrm = app.Services.GetRequiredService<MainWindow>();
+
+            mainFrm.Show();
+        }
+    }
+}
---
+++ DIProjectsApp/WPF/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using DIDemoLib;
+using Microsoft.Extensions.Configuration;
+using System.Windows;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IConfiguration _config;
+        private readonly IMessages _messages;
+
+        public MainWindow(IConfiguration config, IMessages messages)
+        {
+            InitializeComponent();
+            _config = config;
+            _messages = messages;
+
+            helloLabel.Text = _messages.SayHello();
+            goodByeLabel.Text = _messages.SayGoodBye();
+            settingLabel.Text = _config.GetValue<string>("test");
+        }
+    }
+}
